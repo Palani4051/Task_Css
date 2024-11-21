57022140e32bdbdc6f987fe3f186cd64e70c1cdc
Palani4051 <150434156+Palani4051@users.noreply.github.com>
2024-11-21 10:06:38 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,19 @@
+let get=document.getElementById("get");
+
+get.addEventListener("click",()=>{
+    alert("New Asset Added");
+});
+
+let button2=document.getElementsByClassName("button2") [0];
+
+
+button2.addEventListener("click",()=>{
+    alert("Asset Value Updated");
+});
+
+
+
+let get1=document.getElementById("get1");
+get1.addEventListener("click",()=>{
+    alert("You Don't have Any invitations");
+});
